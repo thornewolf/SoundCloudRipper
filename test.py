5dc53ea6777c245b792a47ba38e61821338196b7
Thorne Wolf <thornecanyon@gmail.com>
2018-10-15 11:50:49 -0700

    forgot to include these changed

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from mp3Ripper import download_url
 
-
-download_url("https://soundcloud.com/chloeburbank/i-dont-wanna-waste-my-time",'i-dont-wanna-waste-my-time')
+#testing download_url
+if not download_url("https://soundcloud.com/chloeburbank/i-dont-wanna-waste-my-time",'i-dont-wanna-waste-my-time'):
+    print('[!] download_url failed')
