cf291848f27739f40922be2d891298ce8a063dfa
Thorne Wolf <thornecanyon@gmail.com>
2018-10-21 02:49:29 -0700

    added downloading of the soundcloud track's art to mp3Ripper.py

--- mp3Ripper.py
+++ mp3Ripper.py
@@ -3,7 +3,7 @@ import requests
 import re
 from urllib.request import urlopen
 
-def download_url(url,filename):
+def download_mp3(url,filename):
     html = requests.get(url)
     track_id = re.search(r'soundcloud://sounds:(.+?)"', html.text)
     final_page = requests.get("https://api.soundcloud.com/i1/tracks/{0}/streams?client_id=LvWovRaJZlWCHql0bISuum8Bd2KX79mb".format(track_id.group(1)))
@@ -22,6 +22,31 @@ def download_url(url,filename):
         print(f'[!] Error downloading mp3 for \n\t{url}')
     return True
 
+def download_img(url,filename):
+    html = requests.get(url)
+    #print(html.text)
+    img_match = re.search(r'<img src="(https://.*\.jpg)',html.text)
+    if img_match:
+        img_url = img_match.group(1)
+    else:
+        return None
+    download(img_url,filename+'.jpg')
+
+
+def download(url, file_name):
+    # open in binary mode
+    with open(file_name, "wb") as file:
+        # get request
+        response = requests.get(url)
+        # write to file
+        file.write(response.content)
+
+def download_soundcloud_files(url, file_name):
+    download_mp3(url,file_name)
+    download_img(url,file_name)
+
+
+
 
 if __name__=='__main__':
-    download_url("https://soundcloud.com/chloeburbank/i-dont-wanna-waste-my-time",'i-dont-wanna-waste-my-time')
+    download_soundcloud_files("https://soundcloud.com/chloeburbank/i-dont-wanna-waste-my-time",'i-dont-wanna-waste-my-time')
